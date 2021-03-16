ba460a8f3f37c664f1aab3c798a74aa6b9b0bb2b
macbook <alv-volkov@ukr.net>
2021-03-16 12:59:53 +0200

    refactor Main

--- src/main/java/com/hillel/task23/Main.java
+++ src/main/java/com/hillel/task23/Main.java
@@ -1,15 +1,19 @@
 package com.hillel.task23;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 public class Main {
     public static void main(String[] args) throws IllegalAccessException, NoSuchFieldException {
         String s = "hello";
         char[] arr = {'A', 'B', 'C', 'D', 'E'};
         Class<String> clazz = String.class;
-        Field field = clazz.getDeclaredField("value");
 
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        Field field = clazz.getDeclaredField("value");
         field.setAccessible(true);
+        modifiersField.setInt(field, field.getModifiers() & ~Modifier.FINAL);
         field.set(s, arr);
         System.out.println(s);
 
