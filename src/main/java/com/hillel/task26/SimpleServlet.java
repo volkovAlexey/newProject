7b25238d5d2974f156491ee75eeb077578d8ce2a
macbook <alv-volkov@ukr.net>
2021-03-16 11:50:30 +0200

    add init param to servlet config

--- src/main/java/com/hillel/task26/SimpleServlet.java
+++ src/main/java/com/hillel/task26/SimpleServlet.java
@@ -1,14 +1,30 @@
 package com.hillel.task26;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Enumeration;
 
 public class SimpleServlet extends HttpServlet {
+    private String paramConfig;
+
+    @Override
+    public void init(ServletConfig config) {
+        paramConfig = config.getInitParameter("blockedQueryParam");
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        Enumeration<String> parameterNames = req.getParameterNames();
+        while (parameterNames.hasMoreElements()) {
+            if (parameterNames.nextElement().equals(paramConfig)) {
+                resp.setStatus(400);
+                return;
+            }
+        }
+
         String param1 = req.getParameter("a");
         String param2 = req.getParameter("b");
         String param3 = req.getParameter("c");
