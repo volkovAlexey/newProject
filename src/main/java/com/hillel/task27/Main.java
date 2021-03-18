71a7df94b2e5e40e47f222eb1beae30074da3197
macbook <alv-volkov@ukr.net>
2021-03-18 20:41:02 +0200

    refactor Main

--- src/main/java/com/hillel/task27/Main.java
+++ src/main/java/com/hillel/task27/Main.java
@@ -12,13 +12,22 @@ public class Main {
         ApplicationContext xmlContext = new ClassPathXmlApplicationContext("beans.xml");
         Deck deckXml = xmlContext.getBean("deck", Deck.class);
 
+        List<Card> deckOfCards = deckXml.getDeckOfCards();
+        System.out.println("Deck by xml config - " + deckOfCards);
+        System.out.println();
+
         List<Card> cardListWithXml = deckXml.mix();
-        System.out.println(cardListWithXml);
+        System.out.println("Mix - " + cardListWithXml);
+        System.out.println();
 
         AnnotationConfigApplicationContext annotationContext = new AnnotationConfigApplicationContext(BeanConfig.class);
         Deck deckAnnotation = annotationContext.getBean(Deck.class);
 
+        List<Card> deckOfCards1 = deckAnnotation.getDeckOfCards();
+        System.out.println("Deck by xml config - " + deckOfCards1);
+        System.out.println();
+
         List<Card> cardListWithAnnotation = deckAnnotation.mix();
-        System.out.println(cardListWithAnnotation);
+        System.out.println("Mix - " + cardListWithAnnotation);
     }
 }
