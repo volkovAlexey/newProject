c9b0543da2e405cc100b6d0085039dfb98b09133
macbook <alv-volkov@ukr.net>
2021-03-19 22:30:28 +0200

    add sqlite dependency

---
+++ src/main/java/com/hillel/task29/services/CitiesService.java
@@ -0,0 +1,12 @@
+package com.hillel.task29.services;
+
+import com.hillel.task29.models.City;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface CitiesService {
+    List<City> getCities() throws SQLException;
+
+    void addCity(City city) throws SQLException;
+}
