af7e4c5fdaa29f4fa839d07f6ee303c2c501cd67
macbook <alv-volkov@ukr.net>
2021-03-19 22:17:42 +0200

    add controller

---
+++ src/main/java/com/hillel/task29/cotrollers/CityController.java
@@ -0,0 +1,10 @@
+package com.hillel.task29.cotrollers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/city")
+public class CityController {
+
+}
