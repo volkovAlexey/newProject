c1b4fb65d43a9420a904bd68016f32c44009d0bf
macbook <alv-volkov@ukr.net>
2021-03-24 12:38:08 +0200

    add hello.jsp

--- src/main/java/com/hillel/task29/cotrollers/CityController.java
+++ src/main/java/com/hillel/task29/cotrollers/CityController.java
@@ -1,10 +1,23 @@
 package com.hillel.task29.cotrollers;
 
+import com.hillel.task29.models.City;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/city")
 public class CityController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getCities(String cityName, Model model) {
+        model.addAttribute("cityName", cityName);
+        return "hello";
+    }
 
 }
---
+++ src/main/webapp/WEB-INF/view/hello.jsp
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<html>
+  <body>
+       <form action="city" method="GET">
+       <input type="text" name="cityName">
+       <button type="submit">Enter</button>
+       </form>
+
+       ${cityName}
+  </body>
+</html>
